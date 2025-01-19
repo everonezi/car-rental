c5dd925b823ce8279a5a3d8aa0b1f3d5c105587a
EVeronezi <everonezi25@gmail.com>
2025-01-19 04:03:48 -0300

    final file
    Added functions to register customers, rent cars and update rental statuses

--- trabalhoCRUD.cpp
+++ trabalhoCRUD.cpp
@@ -6,6 +6,7 @@
 int indiceCliente = 0;
 
 
+//Criação das structs
 typedef struct{
 	char placa [10];
 	char marca [10];
@@ -22,6 +23,7 @@ typedef struct{
 	
 }cliente;
 
+//Exibe a lista de carros, dando as informações, como por exemplo, se o carro está disponível ou não
 void consultarCarros(carro listaCarros[]){
 	int i;
 	
@@ -34,19 +36,20 @@ void consultarCarros(carro listaCarros[]){
 	}
 }
 
+//Função para receber uma entrada do usuário para controlar o fluxo do programa
 int receberEntradaDoUsuario(int entrada){
-	printf("\n\nO que deseja fazer?\n1 - Fazer um aluguel\n2 - Exibir a lista de carros\n3 - Atualizar a situacao de um aluguel\n4 - Deletar um aluguel\n5 - Cadastrar um novo cliente\n6 - Exibir lista de clientes cadastrados\n0 - Encerrar o programa\n");
+	printf("\n\nO que deseja fazer?\n1 - Fazer um aluguel\n2 - Exibir a lista de carros\n3 - Atualizar a situacao de um aluguel\n4 - Cadastrar um novo cliente\n5 - Exibir lista de clientes cadastrados\n0 - Encerrar o programa\n");
 	scanf("%d", &entrada);
 	return entrada;
 }
 
 void cadastrarCliente(cliente listaClientes[]) {
-    printf("Nome do cliente: ");
+    printf("Nome do cliente:");
     fflush(stdin); // Limpa o buffer de entrada
     fgets(listaClientes[indiceCliente].nome, sizeof(listaClientes[indiceCliente].nome), stdin);
     listaClientes[indiceCliente].nome[strcspn(listaClientes[indiceCliente].nome, "\n")] = '\0'; // Remove o '\n'
 
-    printf("CPF: ");
+    printf("CPF:");
     fflush(stdin); // Limpa o buffer de entrada
     fgets(listaClientes[indiceCliente].cpf, sizeof(listaClientes[indiceCliente].cpf), stdin);
     listaClientes[indiceCliente].cpf[strcspn(listaClientes[indiceCliente].cpf, "\n")] = '\0'; // Remove o '\n'
@@ -64,6 +67,52 @@ void cadastrarCliente(cliente listaClientes[]) {
     indiceCliente++;
 }
 
+//Função para alugar o carro
+void AlugarCarro(cliente listaCliente[], carro listaCarros[]){
+	int i,j;
+	char placa[10];
+	char cpfCliente[12];
+	int clienteNcadastrado=1;
+	
+	consultarCarros(listaCarros);
+	
+	printf("Informe a placa do veiculo que deseja alugar: ");
+	fflush(stdin);
+	fgets(placa,sizeof(placa),stdin);
+	placa[strcspn(placa, "\n")] = 0;
+	
+	printf("\nInforme o CPF do cliente que deseja alugar o carro com a placa %s\n", placa);
+	fflush(stdin);
+	fgets(cpfCliente,sizeof(cpfCliente),stdin);
+	cpfCliente[strcspn(cpfCliente, "\n")] = 0;
+	
+	//Consulta se o cliente esta cadastrado
+	for(i=0;i<5;i++){
+		if(strcmp(listaCliente[i].cpf,cpfCliente)==0){
+			clienteNcadastrado = 0;
+		}
+	}
+	if(clienteNcadastrado>0){
+		printf("Cliente nao cadastrado !!! Por favor realize o cadastro\n\n");
+		cadastrarCliente(listaCliente);
+	}
+	
+	//Verifica se o carro esta disponivel e realiza o aluguel
+	for(i=0;i<5;i++){
+		if(strcmp(listaCarros[i].situacao,"Disponivel")==0){
+			if(strcmp(listaCarros[i].placa,placa)==0){
+				strcpy(listaCarros[i].situacao,"Alugado");
+				strcat(listaCarros[i].situacao," por ");
+				strcat(listaCarros[i].situacao,cpfCliente);
+				printf("Veiculo com a placa %s alugado com sucesso pelo cliente com CPF %s!\n\n", placa, cpfCliente);
+			}
+		}else{
+			printf("Veiculo Indisponivel!!");
+		}
+	}
+}
+
+//Exibe a lista de clientes cadastrados
 void exibirClientes(cliente listaCliente[]) {
     int i;
     printf("\nLista de clientes cadastrados: \n");
@@ -75,10 +124,29 @@ void exibirClientes(cliente listaCliente[]) {
     }
 }
 
-
-
-
-
+//Atualiza o aluguel de um carro com base na placa
+void atualizarAluguel(carro listaCarros[]){
+	char placaAlterar[10];
+	char atualizarSituacao[15];
+	int i;
+	
+	printf("Digite a placa do carro que deseja alterar a situacao: ");
+	fflush(stdin);
+	fgets(placaAlterar, sizeof(placaAlterar), stdin);
+	placaAlterar[strcspn(placaAlterar, "\n")] = 0;
+	
+	for(i=0;i<5;i++){
+		if(strcmp(listaCarros[i].placa,placaAlterar)==0){
+			printf("\nSituacao do carro: ('Disponivel' ou 'Alugado')");
+			fflush(stdin);
+			fgets(atualizarSituacao, sizeof(atualizarSituacao), stdin);
+			atualizarSituacao[strcspn(atualizarSituacao, "\n")] = 0;
+			break;
+		}
+	}
+	
+	strcpy(listaCarros[i].situacao, atualizarSituacao);
+}
 
 int main(){
 	
@@ -86,6 +154,7 @@ int main(){
 	carro listaCarros[5];
 	cliente listaClientes[5];
 	
+	//Cadastro de 5 carros disponiveis
 	strcpy(listaCarros[0].placa, "ABC1D23");
 	strcpy(listaCarros[0].marca, "Chevrolet");
 	listaCarros[0].ano = 2009;
@@ -117,13 +186,14 @@ int main(){
 	strcpy(listaCarros[4].situacao, "Disponivel"); 
 	
 	
-	printf("\nO que deseja fazer?\n1 - Fazer um aluguel\n2 - Exibir a lista de carros\n3 - Atualizar a situacao de um aluguel\n4 - Deletar um aluguel\n5 - Cadastrar um novo cliente\n6 - Exibir lista de clientes cadastrados\n0 - Encerrar o programa\n");
+	//Recebe uma entrada do usuário para controlar o programa
+	printf("\nO que deseja fazer?\n1 - Fazer um aluguel\n2 - Exibir a lista de carros\n3 - Atualizar a situacao de um aluguel\n4 - Cadastrar um novo cliente\n5 - Exibir lista de clientes cadastrados\n0 - Encerrar o programa\n");
 	scanf("%d", &entrada);
 	
 	while(entrada!= 0){
 		switch(entrada){
 			case 1:
-				
+				AlugarCarro(listaClientes,listaCarros);
 				break;
 				
 			case 2:
@@ -131,18 +201,14 @@ int main(){
 				break;
 				
 			case 3:
-				
+				atualizarAluguel(listaCarros);
 				break;
 				
 			case 4:
-				
-				break;
-				
-			case 5:
 				cadastrarCliente(listaClientes);
 				break;
 				
-			case 6:
+			case 5:
 				exibirClientes(listaClientes);
 				break;
 		}
